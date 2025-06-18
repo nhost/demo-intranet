3aef3b3ceee6c13553e4868a5eb9fdcd1447e29c
David Barroso <dbarrosop@dravetech.com>
2025-06-18 14:45:05 +0200

    asd

--- src/lib/graphql/__generated__/graphql.ts
+++ src/lib/graphql/__generated__/graphql.ts
@@ -35,9 +35,11 @@ export type Scalars = {
 	bigint: { input: number; output: number };
 	bytea: { input: Buffer; output: Buffer };
 	citext: { input: string; output: string };
+	float8: { input: any; output: any };
 	jsonb: { input: Record<string, any>; output: Record<string, any> };
 	numeric: { input: any; output: any };
 	timestamptz: { input: string; output: string };
+	timestampz: { input: any; output: any };
 	uuid: { input: string; output: string };
 };
 
@@ -937,6 +939,458 @@ export enum Files_Update_Column {
 	Placeholder = "_PLACEHOLDER",
 }
 
+export type GraphiteAssistant = {
+	__typename?: "graphiteAssistant";
+	/** ID of the assistant */
+	assistantID: Scalars["String"]["output"];
+	/** Description of the assistant */
+	description: Scalars["String"]["output"];
+	/** File Stores the assistant has access to */
+	fileStores?: Maybe<Array<Scalars["uuid"]["output"]>>;
+	/** GraphQL data sources and tools. Run against the project's GraphQL API */
+	graphql?: Maybe<Array<GraphiteAssistantToolGraphQl>>;
+	/**
+	 * Instructions for the assistant. This is used to instruct the AI assistant
+	 * on how to behave and respond to the user
+	 */
+	instructions: Scalars["String"]["output"];
+	/** Model to use for the assistant. */
+	model: Scalars["String"]["output"];
+	/** Name of the assistant */
+	name: Scalars["String"]["output"];
+	/** Webhook data sources and tools */
+	webhooks?: Maybe<Array<GraphiteAssistantToolWebhook>>;
+};
+
+export type GraphiteAssistantInput = {
+	/** Description of the assistant */
+	description: Scalars["String"]["input"];
+	/** File Stores this assistant has access to */
+	fileStores?: InputMaybe<Array<Scalars["uuid"]["input"]>>;
+	/** GraphQL data sources. Run against the project's GraphQL API */
+	graphql?: InputMaybe<Array<InputMaybe<GraphiteAssistantToolGraphQlInput>>>;
+	/**
+	 * Instructions for the assistant. This is used to instruct the AI assistant
+	 * on how to behave and respond to the user
+	 */
+	instructions: Scalars["String"]["input"];
+	/** Model to use for the assistant. */
+	model: Scalars["String"]["input"];
+	/** Name of the assistant */
+	name: Scalars["String"]["input"];
+	/** Webhook data sources */
+	webhooks?: InputMaybe<Array<InputMaybe<GraphiteAssistantToolWebhookInput>>>;
+};
+
+export type GraphiteAssistantToolArgument = {
+	__typename?: "graphiteAssistantToolArgument";
+	description: Scalars["String"]["output"];
+	name: Scalars["String"]["output"];
+	required: Scalars["Boolean"]["output"];
+	type: Scalars["String"]["output"];
+};
+
+export type GraphiteAssistantToolArgumentInput = {
+	/** Description of the argument. Be as clear and concise as possible. */
+	description: Scalars["String"]["input"];
+	/** Name of the argument */
+	name: Scalars["String"]["input"];
+	/** Whether or not the argument is required */
+	required: Scalars["Boolean"]["input"];
+	/** Type of the argument */
+	type: Scalars["String"]["input"];
+};
+
+export type GraphiteAssistantToolGraphQl = {
+	__typename?: "graphiteAssistantToolGraphQL";
+	/** Arguments to pass to the GraphQL query */
+	arguments: Array<GraphiteAssistantToolArgument>;
+	/** Description of the data source */
+	description: Scalars["String"]["output"];
+	/** Name of the data source */
+	name: Scalars["String"]["output"];
+	/** GraphQL query to run against the project's GraphQL API. */
+	query: Scalars["String"]["output"];
+};
+
+export type GraphiteAssistantToolGraphQlInput = {
+	/** Arguments to pass to the GraphQL query */
+	arguments: Array<GraphiteAssistantToolArgumentInput>;
+	/**
+	 * Description of the data source. Be as clear and concise as possible.
+	 * This is used to help the AI assistant understand when and how
+	 * external data sources should be used.
+	 */
+	description: Scalars["String"]["input"];
+	/** Name of the data source. Use a descriptive name */
+	name: Scalars["String"]["input"];
+	/** GraphQL query to run against the project's GraphQL API. */
+	query: Scalars["String"]["input"];
+};
+
+export type GraphiteAssistantToolWebhook = {
+	__typename?: "graphiteAssistantToolWebhook";
+	/** URL of the webhook */
+	URL: Scalars["String"]["output"];
+	/** Arguments to pass to the webhook */
+	arguments: Array<GraphiteAssistantToolArgument>;
+	/** Description of the data source */
+	description: Scalars["String"]["output"];
+	/** Name of the data source */
+	name: Scalars["String"]["output"];
+};
+
+export type GraphiteAssistantToolWebhookInput = {
+	/** URL of the webhook */
+	URL: Scalars["String"]["input"];
+	/** Arguments to pass to the webhook */
+	arguments: Array<GraphiteAssistantToolArgumentInput>;
+	/**
+	 * Description of the data source. Be as clear and concise as possible.
+	 * This is used to help the AI assistant understand when and how
+	 * external data sources should be used.
+	 */
+	description: Scalars["String"]["input"];
+	/** Name of the data source. Use a descriptive name */
+	name: Scalars["String"]["input"];
+};
+
+export type GraphiteFileStore = {
+	__typename?: "graphiteFileStore";
+	/** IDs of the storage buckets */
+	buckets: Array<Scalars["String"]["output"]>;
+	/** ID of the File Store */
+	id: Scalars["uuid"]["output"];
+	/**
+	 * The last time files in storage were synced with the vector store
+	 * Null means the store hasn't been synced yet
+	 */
+	last_synced_at: Scalars["timestampz"]["output"];
+	/** Name of the File Store */
+	name: Scalars["String"]["output"];
+	/** ID of the vector store */
+	vectorStoreID: Scalars["String"]["output"];
+};
+
+export type GraphiteFileStoreInput = {
+	/** IDs of the storage buckets */
+	buckets: Array<Scalars["String"]["input"]>;
+	/** Name of the File Store */
+	name: Scalars["String"]["input"];
+};
+
+export type GraphiteMessage = {
+	__typename?: "graphiteMessage";
+	/** Timestamp of when the message was sent */
+	createdAt: Scalars["timestampz"]["output"];
+	/** ID of the message */
+	id: Scalars["String"]["output"];
+	/** Message content */
+	message: Scalars["String"]["output"];
+	/** Role of the message. Either "user" or "assistant" */
+	role: Scalars["String"]["output"];
+};
+
+export type GraphiteMessageResponse = {
+	__typename?: "graphiteMessageResponse";
+	/** Messages in this session */
+	messages: Array<GraphiteMessage>;
+	/** ID of the session */
+	sessionID: Scalars["String"]["output"];
+};
+
+export type GraphiteMutation = {
+	__typename?: "graphiteMutation";
+	/**
+	 * Delete an assistant
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (assistants):
+	 * - assistant_id
+	 * delete (assistants):
+	 */
+	deleteAssistant: Scalars["Boolean"]["output"];
+	/**
+	 * Delete a File Store
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (file_stores):
+	 * - vector_store_id
+	 * delete (file_stores):
+	 */
+	deleteFileStore: Scalars["Boolean"]["output"];
+	/**
+	 * Delete a session
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - assistant_id
+	 * delete (sessions):
+	 */
+	deleteSession: Scalars["Boolean"]["output"];
+	/**
+	 * Create an assistant
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (assistants):
+	 * - id
+	 * insert (assistants):
+	 * - user_id
+	 * update (assistants):
+	 * - assistant_id
+	 * - data
+	 */
+	insertAssistant: GraphiteAssistant;
+	/**
+	 * Create a File Store
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (file_stores):
+	 * - id
+	 * insert (file_stores):
+	 * - user_id
+	 */
+	insertFileStore: GraphiteFileStore;
+	/**
+	 * Send a message to a developer session.
+	 * If prevMessageID is `""`, return all messages in the session.
+	 * If prevMessageID is not `""`, return all messages after prevMessageID.
+	 *
+	 * ## Permissions needed
+	 *
+	 * Only admins can send messages to developer sessions
+	 */
+	sendDevMessage: GraphiteMessageResponse;
+	/**
+	 * Send a message to a session.
+	 * If prevMessageID is "", return all messages in the session.
+	 * If prevMessageID is not "", return all messages after prevMessageID.
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - id
+	 * - session_id
+	 * - assistant_id
+	 * update (sessions):
+	 * - update_at
+	 * select (assistants):
+	 * - id
+	 * - data
+	 */
+	sendMessage: GraphiteMessageResponse;
+	/**
+	 * Create a developer session
+	 *
+	 * ## Permissions needed
+	 *
+	 * Only admins can create developer sessions
+	 */
+	startDevSession: GraphiteSession;
+	/**
+	 * Create a session with a given assistant
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - id
+	 * insert (sessions):
+	 * - user_id
+	 * - assistant_id
+	 *
+	 * select (assistants):
+	 * - id
+	 * - data
+	 */
+	startSession: GraphiteSession;
+	/**
+	 * Update an assistant
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (assistants):
+	 * - assistant_id
+	 * update (assistants):
+	 * - update_at
+	 * - data
+	 */
+	updateAssistant: GraphiteAssistant;
+	/**
+	 * Update a File Store
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (file_stores):
+	 * - id
+	 * update (file_stores):
+	 * - updated_at
+	 * - name
+	 */
+	updateFileStore: GraphiteFileStore;
+};
+
+export type GraphiteMutationDeleteAssistantArgs = {
+	assistantID: Scalars["String"]["input"];
+};
+
+export type GraphiteMutationDeleteFileStoreArgs = {
+	id: Scalars["uuid"]["input"];
+};
+
+export type GraphiteMutationDeleteSessionArgs = {
+	sessionID: Scalars["String"]["input"];
+};
+
+export type GraphiteMutationInsertAssistantArgs = {
+	object: GraphiteAssistantInput;
+};
+
+export type GraphiteMutationInsertFileStoreArgs = {
+	object: GraphiteFileStoreInput;
+};
+
+export type GraphiteMutationSendDevMessageArgs = {
+	message: Scalars["String"]["input"];
+	prevMessageID: Scalars["String"]["input"];
+	sessionID: Scalars["String"]["input"];
+};
+
+export type GraphiteMutationSendMessageArgs = {
+	message: Scalars["String"]["input"];
+	prevMessageID: Scalars["String"]["input"];
+	sessionID: Scalars["String"]["input"];
+};
+
+export type GraphiteMutationStartSessionArgs = {
+	assistantID: Scalars["String"]["input"];
+};
+
+export type GraphiteMutationUpdateAssistantArgs = {
+	assistantID: Scalars["String"]["input"];
+	object: GraphiteAssistantInput;
+};
+
+export type GraphiteMutationUpdateFileStoreArgs = {
+	id: Scalars["uuid"]["input"];
+	object: GraphiteFileStoreInput;
+};
+
+export type GraphiteQuery = {
+	__typename?: "graphiteQuery";
+	/**
+	 * Retrieve an assistant
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (assistants):
+	 * - id
+	 * - assistantID
+	 * - data
+	 */
+	assistant?: Maybe<GraphiteAssistant>;
+	/**
+	 * Retrieve all assistants
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (assistants):
+	 * - id
+	 * - assistant_id
+	 * - data
+	 */
+	assistants: Array<GraphiteAssistant>;
+	/**
+	 * Retrieve all File Stores
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (file_stores):
+	 * - id
+	 * - fileStoreID
+	 * - bucketID
+	 * - name
+	 * - last_synced_at
+	 */
+	fileStores: Array<GraphiteFileStore>;
+	/**
+	 * Retrieve a session
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - id
+	 * - assistant_id
+	 * - session_id
+	 * - user_id
+	 */
+	session?: Maybe<GraphiteSession>;
+	/**
+	 * Retrieve all messages for a session
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - id
+	 * - assistant_id
+	 * - session_id
+	 * - user_id
+	 */
+	sessionMessages?: Maybe<GraphiteMessageResponse>;
+	/**
+	 * Retrieve all sessions
+	 *
+	 * ## Permissions needed
+	 *
+	 * select (sessions):
+	 * - id
+	 * - assistant_id
+	 * - session_id
+	 * - user_id
+	 */
+	sessions: Array<GraphiteSession>;
+};
+
+export type GraphiteQueryAssistantArgs = {
+	assistantID: Scalars["String"]["input"];
+};
+
+export type GraphiteQuerySessionArgs = {
+	sessionID: Scalars["String"]["input"];
+};
+
+export type GraphiteQuerySessionMessagesArgs = {
+	sessionID: Scalars["String"]["input"];
+};
+
+export type GraphiteSession = {
+	__typename?: "graphiteSession";
+	/** ID of the assistant used in this session */
+	assistantID: Scalars["String"]["output"];
+	/** Messages in this session */
+	createdAt: Scalars["timestampz"]["output"];
+	/** ID of the session */
+	sessionID: Scalars["String"]["output"];
+	/** ID of the user who started this session */
+	userID: Scalars["uuid"]["output"];
+};
+
+export type Graphite_Search_Kb_Entries_Args = {
+	amount?: InputMaybe<Scalars["Int"]["input"]>;
+	maxdistance?: InputMaybe<Scalars["float8"]["input"]>;
+	query?: InputMaybe<Scalars["String"]["input"]>;
+};
+
+export type Graphite_Similar_Kb_Entries_Args = {
+	amount?: InputMaybe<Scalars["Int"]["input"]>;
+	id?: InputMaybe<Scalars["uuid"]["input"]>;
+	maxdistance?: InputMaybe<Scalars["float8"]["input"]>;
+};
+
 export type Jsonb_Cast_Exp = {
 	String?: InputMaybe<String_Comparison_Exp>;
 };
@@ -1276,6 +1730,7 @@ export type Mutation_Root = {
 	delete_user_departments?: Maybe<User_Departments_Mutation_Response>;
 	/** delete single row from the table: "user_departments" */
 	delete_user_departments_by_pk?: Maybe<User_Departments>;
+	graphite?: Maybe<GraphiteMutation>;
 	/** insert a single row into the table: "storage.files" */
 	insertFile?: Maybe<Files>;
 	/** insert data into the table: "storage.files" */
@@ -1579,6 +2034,11 @@ export type Query_Root = {
 	file?: Maybe<Files>;
 	/** fetch data from the table: "storage.files" */
 	files: Array<Files>;
+	graphite?: Maybe<GraphiteQuery>;
+	/** execute function "graphite_search_kb_entries" which returns "kb_entries" */
+	graphiteSearchKbEntries: Array<Kb_Entries>;
+	/** execute function "graphite_similar_kb_entries" which returns "kb_entries" */
+	graphiteSimilarKbEntries: Array<Kb_Entries>;
 	/** fetch data from the table: "kb_entries" */
 	kb_entries: Array<Kb_Entries>;
 	/** fetch data from the table: "kb_entries" using primary key columns */
@@ -1669,6 +2129,24 @@ export type Query_RootFilesArgs = {
 	where?: InputMaybe<Files_Bool_Exp>;
 };
 
+export type Query_RootGraphiteSearchKbEntriesArgs = {
+	args: Graphite_Search_Kb_Entries_Args;
+	distinct_on?: InputMaybe<Array<Kb_Entries_Select_Column>>;
+	limit?: InputMaybe<Scalars["Int"]["input"]>;
+	offset?: InputMaybe<Scalars["Int"]["input"]>;
+	order_by?: InputMaybe<Array<Kb_Entries_Order_By>>;
+	where?: InputMaybe<Kb_Entries_Bool_Exp>;
+};
+
+export type Query_RootGraphiteSimilarKbEntriesArgs = {
+	args: Graphite_Similar_Kb_Entries_Args;
+	distinct_on?: InputMaybe<Array<Kb_Entries_Select_Column>>;
+	limit?: InputMaybe<Scalars["Int"]["input"]>;
+	offset?: InputMaybe<Scalars["Int"]["input"]>;
+	order_by?: InputMaybe<Array<Kb_Entries_Order_By>>;
+	where?: InputMaybe<Kb_Entries_Bool_Exp>;
+};
+
 export type Query_RootKb_EntriesArgs = {
 	distinct_on?: InputMaybe<Array<Kb_Entries_Select_Column>>;
 	limit?: InputMaybe<Scalars["Int"]["input"]>;
@@ -1755,6 +2233,10 @@ export type Subscription_Root = {
 	files: Array<Files>;
 	/** fetch data from the table in a streaming manner: "storage.files" */
 	files_stream: Array<Files>;
+	/** execute function "graphite_search_kb_entries" which returns "kb_entries" */
+	graphiteSearchKbEntries: Array<Kb_Entries>;
+	/** execute function "graphite_similar_kb_entries" which returns "kb_entries" */
+	graphiteSimilarKbEntries: Array<Kb_Entries>;
 	/** fetch data from the table: "kb_entries" */
 	kb_entries: Array<Kb_Entries>;
 	/** fetch data from the table: "kb_entries" using primary key columns */
@@ -1887,6 +2369,24 @@ export type Subscription_RootFiles_StreamArgs = {
 	batch_size: Scalars["Int"]["input"];
 	cursor: Array<InputMaybe<Files_Stream_Cursor_Input>>;
 	where?: InputMaybe<Files_Bool_Exp>;
+};
+
+export type Subscription_RootGraphiteSearchKbEntriesArgs = {
+	args: Graphite_Search_Kb_Entries_Args;
+	distinct_on?: InputMaybe<Array<Kb_Entries_Select_Column>>;
+	limit?: InputMaybe<Scalars["Int"]["input"]>;
+	offset?: InputMaybe<Scalars["Int"]["input"]>;
+	order_by?: InputMaybe<Array<Kb_Entries_Order_By>>;
+	where?: InputMaybe<Kb_Entries_Bool_Exp>;
+};
+
+export type Subscription_RootGraphiteSimilarKbEntriesArgs = {
+	args: Graphite_Similar_Kb_Entries_Args;
+	distinct_on?: InputMaybe<Array<Kb_Entries_Select_Column>>;
+	limit?: InputMaybe<Scalars["Int"]["input"]>;
+	offset?: InputMaybe<Scalars["Int"]["input"]>;
+	order_by?: InputMaybe<Array<Kb_Entries_Order_By>>;
+	where?: InputMaybe<Kb_Entries_Bool_Exp>;
 };
 
 export type Subscription_RootKb_EntriesArgs = {
